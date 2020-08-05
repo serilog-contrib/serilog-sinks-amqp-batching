baf76653ddfd5930046ebfabebffc85e62087522
Kenan <ki@cybot.com>
2020-08-05 16:14:26 +0200

    Updated namespace

--- Build.ps1
+++ Build.ps1
@@ -1,7 +1,7 @@
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
-dotnet restore .\src\Serilog.Sinks.AMQP\Serilog.Sinks.AMQP.Batching.csproj
-dotnet build .\src\Serilog.Sinks.AMQP\Serilog.Sinks.AMQP.Batching.csproj
+dotnet restore .\src\Serilog.Sinks.AMQP.Batching\Serilog.Sinks.AMQP.Batching.csproj
+dotnet build .\src\Serilog.Sinks.AMQP.Batching\Serilog.Sinks.AMQP.Batching.csproj
 
-echo "RUNNING dotnet pack .\src\Serilog.Sinks.AMQP\Serilog.Sinks.AMQP.Batching.csproj -c Release -o .\artifacts"
-dotnet pack .\src\Serilog.Sinks.AMQP\Serilog.Sinks.AMQP.Batching.csproj -c Release -o .\artifacts
+echo "RUNNING dotnet pack .\src\Serilog.Sinks.AMQP.Batching\Serilog.Sinks.AMQP.Batching.csproj -c Release -o .\artifacts"
+dotnet pack .\src\Serilog.Sinks.AMQP.Batching\Serilog.Sinks.AMQP.Batching.csproj -c Release -o .\artifacts
